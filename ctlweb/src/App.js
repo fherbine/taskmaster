db37131f921219de0480f3dbf670f8578a502fb6
Lucien Lacaze <lacaze.lulu@gmail.com>
2020-11-08 01:48:17 +0100

    adding webctl

---
+++ ctlweb/src/App.js
@@ -0,0 +1,86 @@
+import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import './App.css';
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const requestOptionsGet = {
+      method: 'GET',
+      mode: 'cors',
+    };
+    fetch("http://localhost:9998/", requestOptionsGet)
+      .then(res => res.json().then((result) => {
+        setItems(result.items)
+      })
+    )
+    //  setItems([{
+    //    "pid": 1,
+    //    "name": "test",
+    //    "uptime": "1h"
+    //  },
+    //  {
+    //   "pid": 2,
+    //   "name": "test2",
+    //   "uptime": "1h"
+    // },
+    // {
+    //   "pid": 3,
+    //   "name": "test3",
+    //   "uptime": "1h"
+    // },
+    // ])
+  }, [])
+
+  const handleClick = (itemName, command) => {
+    console.log(itemName, command);
+    const requestOptions = {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify({ command, args: [itemName] })
+    };
+    fetch('http://localhost:9998/', requestOptions)
+        .then(response => {
+          response.json().then((data) => {
+            console.log(data)
+          })
+        })
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to TASKMASTER
+        </p>
+      </header>
+      <div className="Tasks-Panel">
+        <div className="Tasks-Daemon-Button">
+          <Button variant="outline-primary" onClick={(e) => handleClick("daemon", "stop")}>Stop daemon</Button>
+          <Button variant="outline-primary" onClick={(e) => handleClick("config", "update")}>Update configuration file</Button>
+        </div>
+        <ListGroup>
+          <h3 style={{ color: "#31E981" }}>
+            Processes
+          </h3>
+            {items &&
+              items.map(item => {
+                return (
+                  <ListGroup.Item className="Task-listgroup">
+                    {item.name} | {item.pid} | {item.uptime} -{' '}
+                    <Button variant="outline-primary" onClick={(e) => handleClick(item.name, "start")}>Start</Button>
+                    <Button variant="outline-primary" onClick={(e) => handleClick(item.name, "restart")}>Restart</Button>
+                    <Button variant="outline-primary" onClick={(e) => handleClick(item.name, "stop")}>Stop</Button>
+                  </ListGroup.Item>
+                );
+              })}
+          </ListGroup>
+      </div>
+    </div>
+  );
+}
+
+export default App;
