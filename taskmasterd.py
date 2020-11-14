e83f6415e95285bba600079aa463e5a1560c7c09
fherbine <felix.herbinet@yahoo.com>
2020-11-14 22:33:33 +0100

    Proper message on Ctrl+C

--- taskmasterd.py
+++ taskmasterd.py
@@ -18,30 +18,33 @@ LOGLEVEL = getattr(logging, os.environ.get('LOGLEVEL', 'INFO'), logging.INFO)
 
 if __name__ == '__main__':
     logger = Logger(level=LOGLEVEL)
-
     try:
-        logger.info('Parsing input configuration...', end='')
-        parser = TaskmasterDaemonParser.from_command_line()
-    except ParseError as e:
-        print("")
-        logger.error(e)
+        try:
+            logger.info('Parsing input configuration...', end='')
+            parser = TaskmasterDaemonParser.from_command_line()
+        except ParseError as e:
+            print("")
+            logger.error(e)
+            sys.exit(-1)
+
+        logger.success('')
+        logger.debug('Configuration retrieved: %s' % parser.configuration)
+        programs = list()
+
+        for program_name, program_params in parser.configuration.get('programs', {}).items():
+            params = copy.deepcopy(program_params)
+            cmd = params.pop('cmd')
+            task = Task(program_name, cmd, **params)
+            programs.append(task)
+
+        logger.success('Tasks initialized.')
+        manager = Manager(programs, parser)
+        signal.signal(signal.SIGHUP, lambda *_: manager.load_tcp_command(
+            {"command": "update"}
+        ))
+    except KeyboardInterrupt:
+        logger.warning('Ctrl+C, quitting program.')
         sys.exit(-1)
-
-    logger.success('')
-    logger.debug('Configuration retrieved: %s' % parser.configuration)
-    programs = list()
-
-    for program_name, program_params in parser.configuration.get('programs', {}).items():
-        params = copy.deepcopy(program_params)
-        cmd = params.pop('cmd')
-        task = Task(program_name, cmd, **params)
-        programs.append(task)
-
-    logger.success('Tasks initialized.')
-    manager = Manager(programs, parser)
-    signal.signal(signal.SIGHUP, lambda *_: manager.load_tcp_command(
-        {"command": "update"}
-    ))
 
     logger.info('Start server on `localhost:9998`')
     server = Server(manager)
